9ff0581221f3b92f15ba82c3839ba832e7887fd4
jasson31 <jasson31@naver.com>
2019-10-30 18:21:54 +0900

    쉐이더 변수 컨트롤 연습

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.Find("Robot2").GetComponent<Renderer>().material.GetColor("_Color"));
+        transform.Find("Robot2").GetComponent<Renderer>().material.SetColor("_Color", new Color(1, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
